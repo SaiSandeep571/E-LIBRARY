cad66ac1e90843987ac50c08e4b206a8ab672d0f
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-04-12 11:41:00 +0530

    fix

--- lib/login.dart
+++ lib/login.dart
@@ -9,78 +9,68 @@ class Loginscreen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(
-                'assets/background.png',
-              ),
-              fit: BoxFit.cover),
-        ),
-        child: Center(
-          child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('EMAIL/MOBILE NO'),
-        
-                  SizedBox(height: 5,),
-        
-        
-                  SizedBox(
-                    height: 30,
-                    width: 250,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder()
-                      ),
+      body: Center(
+        child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('EMAIL/MOBILE NO'),
+
+                SizedBox(height: 5,),
+
+
+                SizedBox(
+                  height: 30,
+                  width: 250,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder()
                     ),
                   ),
-        
-                  SizedBox(height: 10,),
-        
-        
-                  Text('PASSWORD'),
-        
-                  SizedBox(height: 5,),
-        
-        
-                  SizedBox(
-                    height: 30,
-                    width: 250,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder()
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-        
-        
-              SizedBox(height: 25,),
-              
-        
-              InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => home()),);
-                },
-                child:Container(
-                alignment: Alignment.center,
-                height: 30,
-                width: 60,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.cyan
                 ),
-                child: Text('LOGIN'),
-              ),)
-              
-            ],
-          ),
+
+                SizedBox(height: 10,),
+
+
+                Text('PASSWORD'),
+
+                SizedBox(height: 5,),
+
+
+                SizedBox(
+                  height: 30,
+                  width: 250,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder()
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+
+            SizedBox(height: 25,),
+            
+
+            InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => home()),);
+              },
+              child:Container(
+              alignment: Alignment.center,
+              height: 30,
+              width: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.cyan
+              ),
+              child: Text('LOGIN'),
+            ),)
+            
+          ],
         ),
       ),
     );
